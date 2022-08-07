1282d499f7e5a003bccb26324181bded8f497ee5
gsaple <yymelau@gmail.com>
2022-08-07 15:48:12 +1000

    wallpaper with sxiv

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-feh --bg-fill --randomize $HOME/wallpapers/*
+case "$1" in
+"r")
+    feh --bg-fill --randomize --no-fehbg $HOME/wallpapers/* ;;
+"w")
+    feh --bg-fill --no-fehbg $2 ;;
+"s")
+    find $HOME/wallpapers -maxdepth 1 -type f | shuf | sxiv - -t
+esac
+
+# TODO Pywal stuff here
