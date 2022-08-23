f34b0ac1ca35f4c64dd1004fa117c93c16492cd4
gsaple <yymelau@gmail.com>
2022-08-23 12:04:35 +1000

    maim

---
+++ screen_shot.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+name="$HOME/screen_shot/$(date +%s_%h%d_%H:%M:%S).png"
+select=$(echo -e "full\nselect\nactive" | dmenu -l 3)
+case $select in
+    "full")
+	maim -o -d 2 $name
+        ;;
+    "select")
+        maim -s -o -d 2 $name
+	;;
+    "active")
+	maim -o -i $(xdotool getactivewindow) $name
+        ;;
+esac
+exit 0
