207b089d8161662d0f71534f070df3da7ed0b26a
gsaple <yymelau@gmail.com>
2023-07-15 14:43:30 +1000

    wallpaper folder

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 case "$1" in
 "r")
-    feh --bg-fill --randomize --no-fehbg $HOME/wallpapers/* ;;
+    feh --bg-fill --randomize --no-fehbg $HOME/Pictures/wallpapers/* ;;
 "w")
     feh --bg-fill --no-fehbg $2 ;;
 "s")
-    find $HOME/wallpapers -maxdepth 1 -type f | shuf | sxiv - -t
+    find $HOME/Pictures/wallpapers -maxdepth 1 -type f | shuf | sxiv - -t
 esac
 
 # TODO Pywal stuff here
